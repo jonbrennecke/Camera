551379419c02738936b691cef9acb6ed8e88e4f4
Jon Brennecke <jpbrennecke@gmail.com>
2020-05-18 12:47:46 -0700

    added script to push pod to private specs repo

---
+++ scripts/push_pod_version.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+version=$(ruby -e "$(head -n 1 Camera.podspec); puts(version.strip);")
+
+git commit -a --allow-empty -m "v$version"
+
+if [ -z "$(git status --porcelain)" ]; then 
+  git tag "v$version"
+  git push origin master
+  git push origin master --tags
+  pod repo push private-pod-specs Camera.podspec --allow-warnings  
+else 
+  echo "Error: Git has uncommitted changes."
+  exit 1
+fi
