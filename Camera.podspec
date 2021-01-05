2b5389bce6698b0b4619ceb9f52ed8c210e1153f
Jon Brennecke <jpbrennecke@gmail.com>
2021-01-05 00:37:49 -0800

    v0.0.14-dev6

--- Camera.podspec
+++ Camera.podspec
@@ -1,4 +1,4 @@
-version = '0.0.14-dev5'
+version = '0.0.14-dev6'
 
 Pod::Spec.new do |s|
   s.name = 'Camera'
--- scripts/push_pod_version.sh
+++ scripts/push_pod_version.sh
@@ -7,7 +7,7 @@ if [ -z "$(git status --porcelain)" ]; then
   git tag "v$version"
   git push origin master
   git push origin master --tags
-  pod repo push private-pod-specs Camera.podspec --allow-warnings  
+  pod repo push private-pod-specs Camera.podspec --allow-warnings --swift-version=5
 else 
   echo "Error: Git has uncommitted changes."
   exit 1
